baae8695586b2f8830a5adbd147d1818446407d8
Bryce Himebaugh <bhimebau@gmail.com>
2022-06-27 08:18:18 -0400

    checkpoint

--- simple_rx.py
+++ simple_rx.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3 
 
+import faulthandler; faulthandler.enable()
 import adi
 sdr = adi.Pluto('ip:192.168.2.1') # or whatever your Pluto's IP is
 sdr.sample_rate = int(2.5e6)
 print(sdr.rx())
-
+print("Hi there")
